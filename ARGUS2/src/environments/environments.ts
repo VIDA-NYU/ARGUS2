8b5976e7076f244600e6a5e063bd142dd1278a69
soniacq <scastelo2@gmail.com>
2024-01-26 10:41:01 -0500

    Using the ENV property in the Dockerfile to initialise the process.env.variable.

--- ARGUS2/src/environments/environments.ts
+++ ARGUS2/src/environments/environments.ts
@@ -1,5 +1,6 @@
 // const SERVERIP: string = 'http://dashboardptg.vida-nyu.org:7890';
-const SERVERIP: string = 'http://192.168.50.223:7890';
+// const SERVERIP: string = 'http://192.168.50.223:7890';
+const SERVERIP: string = process.env["PTG_URL"];
 
 export const environment = {
     
--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,6 @@ FROM nginx:stable-alpine
 COPY --from=build /src/* /usr/share/nginx/html
 ##COPY nginx.conf /etc/nginx/conf.d/default.conf
 
-
+ENV PTG_URL=http://api:8000
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
